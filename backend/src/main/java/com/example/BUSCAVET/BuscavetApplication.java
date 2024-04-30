ef317c485fd85f0469df0eeae1726897b33efe90
Orlando Solís <orlando.solis.d@usach.cl>
2024-04-30 00:47:10 -0400

    Inicialización del backend y configuración de la base de datos

---
+++ backend/src/main/java/com/example/BUSCAVET/BuscavetApplication.java
@@ -0,0 +1,13 @@
+package com.example.BUSCAVET;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscavetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscavetApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/BUSCAVET/BuscavetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BUSCAVET;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscavetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
