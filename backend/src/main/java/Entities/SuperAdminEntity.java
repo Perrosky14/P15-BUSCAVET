c32e8dccb92557d0d35099e35acc2613fc2a2f01
Orlando Solís Domínguez <orlando.solis.d@usach.cl>
2024-05-01 18:26:44 -0400

    [Backend] Entidad superAdmin

--- backend/src/main/java/Entities/SuperAdminEntity.java
+++ backend/src/main/java/Entities/SuperAdminEntity.java
@@ -1,4 +1,23 @@
 package Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Table(name = "SuperAdmin")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class SuperAdminEntity {
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String correo;
+    private String contrasenia;
 }
