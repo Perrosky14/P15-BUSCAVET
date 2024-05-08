0b192a2004d35ca55a9358f10fcc31a173c8a97e
Orlando Solís <orlando.solis.d@usach.cl>
2024-05-08 15:23:47 -0400

    [Backend] Registrar veterinaria desde superAdmin

--- backend/src/main/java/com/example/BUSCAVET/Controllers/VeterinariaController.java
+++ backend/src/main/java/com/example/BUSCAVET/Controllers/VeterinariaController.java
@@ -1,8 +1,12 @@
 package com.example.BUSCAVET.Controllers;
 
+import com.example.BUSCAVET.Entities.SuperAdminEntity;
 import com.example.BUSCAVET.Entities.VeterinariaEntity;
+import com.example.BUSCAVET.Services.SuperAdminService;
 import com.example.BUSCAVET.Services.VeterinariaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,6 +16,9 @@ public class VeterinariaController {
 
     @Autowired
     VeterinariaService veterinariaService;
+
+    @Autowired
+    SuperAdminService superAdminService;
 
     @GetMapping("/")
     public ArrayList<VeterinariaEntity> obtenerVeterinaria(){return veterinariaService.obtenerVeterinarias();}
@@ -38,6 +45,18 @@ public class VeterinariaController {
     @DeleteMapping("/{id}")
     public void eliminarVeterinaria(@PathVariable Long id){
         veterinariaService.eliminarVeterinaria(id);
+    }
+
+    @PostMapping("/{idSuperAdmin}")
+    public ResponseEntity<?> registrarVeterinaria(@PathVariable Long idSuperAdmin, @RequestBody VeterinariaEntity veterinaria){
+        SuperAdminEntity superAdmin = superAdminService.obtenerPorId(idSuperAdmin);
+        if (superAdmin != null) {
+            veterinaria.setSuperAdmin(superAdmin);
+            veterinariaService.guardarVeterinaria(veterinaria);
+            return ResponseEntity.status(HttpStatus.CREATED).body("La veterinaria " + veterinaria.getNombre_comercial() + " ha sido registrada correctamente por el superAdmin con el ID: " + idSuperAdmin);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se ha encontrado ningun superAdmin que tenga el id: " + idSuperAdmin);
+        }
     }
 
 }
