97a102a1818c434a6828942fabeeef905679b882
Orlando Solís <orlando.solis.d@usach.cl>
2024-05-08 12:43:20 -0400

    [Backend] Registrar doctor desde veterinaria

--- backend/src/main/java/com/example/BUSCAVET/Controllers/DoctorController.java
+++ backend/src/main/java/com/example/BUSCAVET/Controllers/DoctorController.java
@@ -1,8 +1,12 @@
 package com.example.BUSCAVET.Controllers;
 
 import com.example.BUSCAVET.Entities.DoctorEntity;
+import com.example.BUSCAVET.Entities.VeterinariaEntity;
 import com.example.BUSCAVET.Services.DoctorService;
+import com.example.BUSCAVET.Services.VeterinariaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -11,6 +15,9 @@ import java.util.ArrayList;
 public class DoctorController {
     @Autowired
     DoctorService doctorService;
+
+    @Autowired
+    VeterinariaService veterinariaService;
 
     @GetMapping("/")
     public ArrayList<DoctorEntity> obtenerDoctores(){
@@ -32,7 +39,6 @@ public class DoctorController {
         return "redirect:/nuevo-doctor";
     }
 
-
     @PutMapping("/{id}")
     public DoctorEntity actualizarDoctor(@PathVariable Long id, @RequestBody DoctorEntity doctorActualizado){
         return doctorService.actualizarDoctor(id, doctorActualizado);
@@ -41,5 +47,17 @@ public class DoctorController {
     @DeleteMapping("/{id}")
     public void eliminarDoctor(@PathVariable Long id){
         doctorService.eliminarDoctor(id);
+    }
+
+    @PostMapping("/{idVeterinaria}")
+    public ResponseEntity<?> registrarDoctor(@PathVariable Long idVeterinaria, @RequestBody DoctorEntity doctor){
+        VeterinariaEntity veterinaria = veterinariaService.obtenerPorId(idVeterinaria);
+        if (veterinaria != null) {
+            doctor.setVeterinaria(veterinaria);
+            doctorService.guardarDoctor(doctor);
+            return ResponseEntity.status(HttpStatus.CREATED).body("El doctor " + doctor.getNombre1() + " " + doctor.getApellido1() + " ha sido registrado correctamente por la veterinaria con el ID: " + idVeterinaria);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se ha encontrado ninguna veterinaria que tenga la id: " + idVeterinaria);
+        }
     }
 }
