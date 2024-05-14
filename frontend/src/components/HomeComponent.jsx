08ea9f2ceb5eccd9bb6516687ed38e601a31c680
Ivan <ivan.zuniga@usach.cl>
2024-05-14 11:54:00 -0400

    [Frontend] Vista Main y Registro V1.0

---
+++ frontend/src/components/HomeComponent.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import styled from "styled-components";
+import { createGlobalStyle } from 'styled-components';
+import NavbarComponent from "./NavbarComponent";
+import Modal from './Modal/Modal';
+import Button from 'react-bootstrap/Button';
+import CloseButton from 'react-bootstrap/CloseButton';
+
+const HomeComponent = () => {
+    const [isModalOpen, setModalOpen] = useState(false);
+  
+    const openModal = () => {
+      setModalOpen(true);
+    };
+  
+    const closeModal = () => {
+      setModalOpen(false);
+    };
+    
+
+    return (
+        <div>
+            <NavbarComponent></NavbarComponent>
+            <GlobalStyle /> 
+            <button onClick={openModal}>Open Modal</button>
+                <Modal isOpen={isModalOpen} onClose={closeModal}>
+                    <Button variant="light">Registro</Button>{' '}
+                    <Button variant="light">Log in</Button>{' '}
+                    <CloseButton aria-label="Hide" onClick={closeModal}/>
+                    <h2>hola desde la modal</h2>
+                </Modal>
+            <h1>hola3</h1>
+            <HomeStyle>
+            </HomeStyle>
+        </div>
+    );
+}
+
+export default HomeComponent;
+
+
+const GlobalStyle = createGlobalStyle`
+    body { 
+        background-color: #FBFBFB;
+`
+const HomeStyle = styled.nav`
+
+.text-center {
+    text-align: center;
+    justify-content: center;
+    padding-top: 8px;
+    color: #fff;
+}
+
+.box-area{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+}
+
+.single-box{
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 400px;
+    height: auto;
+    border-radius: 4px;
+    background-color: #fff;
+    text-align: center;
+    margin: 20px;
+    padding: 20px;
+    transition: .3s
+}
+
+
+`
