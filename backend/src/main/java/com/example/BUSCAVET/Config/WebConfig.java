b3ff26a565702ad08c1f7c42cf08d12ac3a0fbfa
Orlando Solís <orlando.solis.d@usach.cl>
2024-05-31 16:42:54 -0400

    [Backend] FIX: nombre del paquete config

--- backend/src/main/java/com/example/BUSCAVET/Config/WebConfig.java
+++ backend/src/main/java/com/example/BUSCAVET/Config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.BUSCAVET.config;
+package com.example.BUSCAVET.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
