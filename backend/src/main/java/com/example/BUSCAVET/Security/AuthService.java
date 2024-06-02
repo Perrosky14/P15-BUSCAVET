959f65d10130ee9f9c3d06591f15f47647334e52
Orlando Solís <orlando.solis.d@usach.cl>
2024-06-02 19:36:04 -0400

    [Backend] ADD: Componentes para JWT

---
+++ backend/src/main/java/com/example/BUSCAVET/Security/AuthService.java
@@ -0,0 +1,14 @@
+package com.example.BUSCAVET.Security;
+
+import com.example.BUSCAVET.Security.AuthResponse;
+import com.example.BUSCAVET.Security.LoginRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    public AuthResponse login(LoginRequest request) {
+        return null;
+    }
+
+}
---
+++ backend/src/main/java/com/example/BUSCAVET/Security/JWTService.java
@@ -0,0 +1,12 @@
+package com.example.BUSCAVET.Security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+
+    public String getTokenUsuario(UserDetails usuario) {
+        return null;
+    }
+}
