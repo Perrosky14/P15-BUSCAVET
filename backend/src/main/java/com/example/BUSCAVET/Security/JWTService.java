6f181ded9632b4812717e59e7776aecc8115d87c
Orlando Solís <orlando.solis.d@usach.cl>
2024-06-02 22:14:08 -0400

    [Backend] ADD & UPDATE: dependencias JWT más configuración de App, creación de token

--- backend/src/main/java/com/example/BUSCAVET/Security/JWTService.java
+++ backend/src/main/java/com/example/BUSCAVET/Security/JWTService.java
@@ -1,12 +1,35 @@
 package com.example.BUSCAVET.Security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JWTService {
 
-    public String getTokenUsuario(UserDetails usuario) {
-        return null;
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String getToken(UserDetails usuario) {
+        return getToken(new HashMap<>(), usuario);
     }
+
+    private String getToken(Map<String, Object> extraClaims, UserDetails user) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
 }
--- backend/src/main/java/com/example/BUSCAVET/Security/SecurityConfig.java
+++ backend/src/main/java/com/example/BUSCAVET/Security/SecurityConfig.java
@@ -3,14 +3,20 @@ package com.example.BUSCAVET.Security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,6 +27,10 @@ public class SecurityConfig {
                                 /*.requestMatchers("/asd").permitAll()*/
                                 .anyRequest().permitAll()
                         )
+                .sessionManagement( sessionManager -> sessionManager
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
